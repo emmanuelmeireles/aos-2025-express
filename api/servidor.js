6325220d215de0d244849eb9f60efd8495c3bbe9
Emmanuel Meireles Silva <emmanuelmeireles@gmail.com>
2025-05-17 21:33:34 +0000

    para dar sorte

--- api/servidor.js
+++ api/servidor.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello, world!');
-    res.send('ola, mundo!');	
+    
+    res.send('ola, mundo! para dar sorte');	
 });
 
 app.listen(port, () => {
